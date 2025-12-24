3e7617a5a0804965493ae2ded20e94d22e81a9ec
Yashpandey0912 <yashpandey39380@gmail.com>
2025-12-24 14:14:34 +0530

    Add files via upload

---
+++ dsatracker.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById("toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        toggleBtn.innerText = "Light Mode";
+    } else {
+        toggleBtn.innerText = "Dark Mode";
+    }
+});
